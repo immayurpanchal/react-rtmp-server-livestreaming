f9bf035e05e932d72b4e1943c30788347514803b
Mayur Panchal <mayur.panchal@vuclip.com>
2019-12-27 15:25:55 +0530

    port changed

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const NodeMediaServer = require("node-media-server");
 
 const config = {
   rtmp: {
-    port: 1935,
+    port: 1936,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
     ping_timeout: 60
   },
   http: {
-    port: process.env.PORT || 8000,
+    port: process.env.PORT || 8001,
     allow_origin: "*"
   }
 };
